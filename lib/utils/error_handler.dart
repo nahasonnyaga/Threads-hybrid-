69f6c3bd5bddbaff6418003a631b206be5ce568e
breakingnews <e35-5321-2020@student.mksu.ac.ke>
2025-11-12 22:53:05 +0300

    Add ErrorHandler class for displaying error messages

--- lib/utils/error_handler.dart
+++ lib/utils/error_handler.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class ErrorHandler {
+  static void show(BuildContext context, {required String message}) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message, style: TextStyle(color: Colors.white)),
+      backgroundColor: Colors.red,
+    ));
+  }
+}
