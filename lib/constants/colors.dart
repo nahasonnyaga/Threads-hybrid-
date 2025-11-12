67743ed6904f425212ac379c042bf74898dffe24
breakingnews <e35-5321-2020@student.mksu.ac.ke>
2025-11-12 22:21:22 +0300

    Create colors.dart

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF1DA1F2);
+  static const Color accent = Color(0xFFFF2D55);
+  static const Color secondary = Color(0xFFF5F8FA);
+  static const Color bgDark = Color(0xFF222831);
+  static const Color bgLight = Color(0xFFF5F8FA);
+  static const Color danger = Color(0xFFD32F2F);
+  static const Color warning = Color(0xFFFFC107);
+  static const Color success = Color(0xFF43A047);
+  static const Color info = Color(0xFF1976D2);
+}
