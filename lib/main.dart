3ad13a9bf839e3f96786ba8143ea9a6f28824c96
breakingnews <e35-5321-2020@student.mksu.ac.ke>
2025-11-12 23:40:17 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'screens/app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // TODO: Add Firebase/Cloudinary/Supabase initialization below.
+  runApp(App());
+}
