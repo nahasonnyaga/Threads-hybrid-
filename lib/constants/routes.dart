b99e77c39f2054111c5fecb88ea467f460843371
breakingnews <e35-5321-2020@student.mksu.ac.ke>
2025-11-12 22:22:50 +0300

    Create routes.dart

---
+++ lib/constants/routes.dart
@@ -0,0 +1,23 @@
+class Routes {
+  static const String home = '/';
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String profile = '/profile';
+  static const String explore = '/explore';
+  static const String reels = '/reels';
+  static const String notifications = '/notifications';
+  static const String uploadPost = '/upload_post';
+  static const String onboarding = '/onboarding';
+  static const String settings = '/settings';
+  static const String premium = '/premium';
+  static const String monetization = '/monetization';
+  static const String jobs = '/jobs';
+  static const String lists = '/lists';
+  static const String spaces = '/spaces';
+  static const String articles = '/articles';
+  static const String highlights = '/highlights';
+  static const String helpFaq = '/help_faq';
+  static const String deleteAccount = '/delete_account';
+  static const String privacyPolicy = '/privacy';
+  static const String terms = '/terms';
+}
