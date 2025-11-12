e74f7e3945e9fad32ea88d3bbbe9f46bf4dd7c1c
breakingnews <e35-5321-2020@student.mksu.ac.ke>
2025-11-12 22:55:59 +0300

    Create theme_constants.dart

---
+++ lib/utils/theme_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kAppBarColor = Color(0xFF15202B);
+const kAccent = Color(0xFFFF2D55);
+const kInputBorderRadius = BorderRadius.all(Radius.circular(18));
+const kPadding16 = EdgeInsets.all(16);
+const kPadding24 = EdgeInsets.all(24);
+const kBgLight = Color(0xFFF5F8FA);
+const kBgDark = Color(0xFF222831);
