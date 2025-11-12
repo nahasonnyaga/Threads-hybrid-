ad0f09f458c6aa7e4834274c969ab036dcb3bf42
breakingnews <e35-5321-2020@student.mksu.ac.ke>
2025-11-12 22:05:51 +0300

    Create custom_icons.dart

---
+++ lib/assets/custom_icons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  static const IconData reel = IconData(0xe900, fontFamily: 'CustomIcons');
+  static const IconData hashtag = IconData(0xe901, fontFamily: 'CustomIcons');
+  static const IconData mention = IconData(0xe902, fontFamily: 'CustomIcons');
+  static const IconData premium = IconData(0xe903, fontFamily: 'CustomIcons');
+  static const IconData chat = IconData(0xe904, fontFamily: 'CustomIcons');
+}
