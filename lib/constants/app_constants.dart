96ba28738e256b7fa254dc15994bd33ac860ede2
breakingnews <e35-5321-2020@student.mksu.ac.ke>
2025-11-12 22:27:26 +0300

    Create app_constants.dart

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+const kAppName = 'Testagram';
+const kSupportEmail = 'support@testagram.com';
+const kDefaultAvatar = 'https://example.com/default_avatar.png';
+const kMaxPostLength = 240;
+const kBannerUrl = 'https://example.com/banner.jpg';
